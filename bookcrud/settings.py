a1dec5aabacd6bdd4b6328e62205206fc3951568
Bryan Lester <bryanlester.fundar@gmail.com>
2026-03-24 20:27:05 +0800

    change db config into mysql

--- bookcrud/settings.py
+++ bookcrud/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'bookcrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER':'root',
+        'PASSWORD': 'Bryan@123!',
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
