fee8927b47124b4c2f0ed55bfbe71db0e795ae01
Bryan Lester <bryanlester.fundar@gmail.com>
2026-03-24 20:31:26 +0800

    add missing db config

--- bookcrud/settings.py
+++ bookcrud/settings.py
@@ -79,6 +79,8 @@ DATABASES = {
         'NAME': 'django_db',
         'USER':'root',
         'PASSWORD': 'Bryan@123!',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
         'OPTIONS':{
             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
         }
