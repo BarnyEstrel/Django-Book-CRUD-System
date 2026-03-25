8e5c0801f1e6c7d91edba0bd5cc33ede2d1352db
Bryan Lester <bryanlester.fundar@gmail.com>
2026-03-25 20:40:55 +0800

    add Genre model

--- books/models.py
+++ books/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     stock = models.IntegerField(default=0)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    created_at = models.DateField(auto_now=True)
