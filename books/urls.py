6120d8cdbf9a96844d72faab50b166ff71caeb7d
Bryan Lester <bryanlester.fundar@gmail.com>
2026-03-26 20:08:38 +0800

    basic form for uploading genre in database

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('function', views.hello_world),
     path('class', views.HelloWarudo.as_view()),
+    path('genre', views.home),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import GenreForm
 # Create your views here.
 def hello_world(request):
     return HttpResponse("Hello World")
@@ -8,3 +9,13 @@ def hello_world(request):
 class HelloWarudo(View):
     def get(self, request):
         return HttpResponse("Hello Warudo")
+
+def home(request):
+    form = GenreForm()
+
+    if request.method == 'POST':
+        form = GenreForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success")
+    return render(request, 'index.html', {'form' : form})
